bfe52fcdcfd67d6c4ccd4063763dd22ff89cf527
Damian Malysza <damian.malysza@gmail.com>
2021-11-23 22:08:07 -0500

    added constructor for recipe class and tested that recipe gets instantiatedproperly with json data

--- js/index.js
+++ js/index.js
@@ -1,6 +1,14 @@
 const recipeIndexUrl = "http://localhost:3000/recipes"
 const mainContainer = document.querySelector(".main-container")
+const recipes = []
+fetchRecipes()
 
-document.addEventListener("DOMContentLoaded",() => {
-  const test = new Recipe
-})
+function fetchRecipes(){
+  fetch(recipeIndexUrl)
+  .then(response => response.json())
+  .then(json => {
+    json.data.forEach(recipe => {
+      recipes.push(new Recipe(recipe))
+    })
+  })
+}
--- js/recipe.js
+++ js/recipe.js
@@ -5,11 +5,18 @@ class Recipe {
   cooktime;
   servings;
   category;
-  ingredients = [];
-  directions = []; 
+  ingredients;
+  directions; 
 
-  static #recipesUrl = "http://localhost:3000/recipes"
-  static recipes = [];
- 
+  constructor(params){
+    this.recipe_id = params.id;
+    this.name = params.attributes.name;
+    this.preptime = params.attributes.preptime
+    this.cooktime = params.attributes.cooktime
+    this.servings = params.attributes.servings
+    this.category = params.attributes.category
+    this.ingredients = params.attributes.ingredients
+    this.directions = params.attributes.directions
+  }
 
 }
