6962feccebdc1349acfe75cd3d15e0b9f7acdb48
Damian Malysza <damian.malysza@gmail.com>
2021-11-24 20:20:34 -0500

    fixed async bug where category tabs weren't made because of fetch call

--- js/index.js
+++ js/index.js
@@ -3,11 +3,12 @@ const categoriesIndexUrl = "http://localhost:3000/categories"
 const mainContainer = document.querySelector(".main-container")
 const recipes = []
 const categories = []
+const navBar = document.querySelector('#nav-tab')
 const tabContent = document.querySelector('#nav-tabcontent')
 
 document.addEventListener("DOMContentLoaded",() => {
   fetchRecipes()
-  fetchCategories()
+  fetchAndCreateCategories()
   let navItems = document.querySelectorAll(".nav-link")
   navItems.forEach(item => {
     item.addEventListener("click", displayCards)
@@ -28,45 +29,61 @@ function fetchRecipes(){
   })
 }
 
-function fetchCategories(){
+function createCategoryTabs(categories) {
+  categories.forEach(category => {
+    let tab = document.createElement('a')
+    tab.className = 'nav-item nav-link'
+    tab.setAttribute('data-toggle', 'tab')
+    tab.setAttribute('category', `${category}`)
+    tab.href = `#nav-${category}`
+    tab.innerText = `${category}`
+    
+    navBar.appendChild(tab)
+  })
+}
+
+
+function fetchAndCreateCategories(){
   fetch(categoriesIndexUrl)
   .then(response => response.json())
   .then(json => {
     json.forEach(category => categories.push(category))
+    createCategoryTabs(categories)
   })
 }
+
 
 function createRecipeCard(recipe) {
   let cardOuterShell = document.createElement('div')
   cardOuterShell.className = 'col-sm-3'
-
+  
   let cardInnerShell = document.createElement('div')
   cardInnerShell.className = 'card mt-3'
   cardOuterShell.appendChild(cardInnerShell)
-
+  
   let cardBody = document.createElement('div')
   cardBody.className = 'card-body text-center'
   cardInnerShell.appendChild(cardBody)
-
+  
   let cardTitle = document.createElement('h5')
   cardTitle.className = 'card-title'
   cardTitle.innerText = recipe.name
-
+  
   let cardSubTitle = document.createElement('h6')
   cardSubTitle.className = 'card-subtitle mb-2 text-muted'
   cardSubTitle.innerText = `${recipe.totalTime} minutes`
-
+  
   let cardText = document.createElement('p')
   cardText.className = 'card-text'
   cardText.innerText = `${recipe.servings} Servings`
-
+  
   let button = document.createElement('button')
   button.className = 'btn btn-primary'
   button.id = recipe.recipe_id
   button.innerText = 'View Recipe'
-
+  
   cardBody.append(cardTitle,cardSubTitle,cardText,button)
-
+  
   return cardOuterShell
 }
 
@@ -74,9 +91,9 @@ function displayCards(event){
   while (tabContent.firstChild) {
     tabContent.removeChild(tabContent.firstChild)
   }
-
+  
   recipes.forEach(recipe => {
     tabContent.appendChild(createRecipeCard(recipe))
   })
-
+  
 }
